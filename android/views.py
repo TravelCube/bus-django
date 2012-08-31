253485fc3830e580712bddb0736ea4f47165444f
omer <omer@omer.com>
2012-08-31 20:51:08 +0300

    add bus modle to the party

--- android/views.py
+++ android/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
+from Bus import bus
 
 def lines(request):
     q = request.GET
-    return HttpResponse(q.items())
+    names = bus.get(1,2,3,4,5)
+    return HttpResponse(names[0])
 
