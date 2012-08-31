1c3e3fdc7c4e6c54304c0c75848530cb248baf58
omer <omer@omer.com>
2012-08-31 21:13:36 +0300

    output is now in json

--- android/views.py
+++ android/views.py
@@ -1,8 +1,11 @@
 from django.http import HttpResponse
 from Bus import bus
+import json
 
 def lines(request):
     q = request.GET
     names = bus.get(1,2,3,4,5)
-    return HttpResponse(names[0])
+    j = json.dumps(names,ensure_ascii=False)
+    #return HttpResponse(j, mimetype="text/json;charset=UTF-8")
+    return HttpResponse(j)
 
