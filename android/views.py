a06d3d312aae0a0e92e40de9de5dd5127f451e5e
omer <omer@omer.com>
2012-08-31 23:17:25 +0300

    formatted json

--- android/views.py
+++ android/views.py
@@ -5,7 +5,8 @@ import json
 def lines(request):
     q = request.GET
     names = bus.get(1,2,3,4,5)
-    j = json.dumps(names,ensure_ascii=False)
+    data = [{'id':key, 'lastStop':value} for key,value in names.items()]
+    j = json.dumps({'data':data},ensure_ascii=False)
     #return HttpResponse(j, mimetype="text/json;charset=UTF-8")
     return HttpResponse(j)
 
