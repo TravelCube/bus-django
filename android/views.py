0238b63eb65c6e4ef00b7baece54f1eacdf5df3e
omer <omer@omer.com>
2012-09-01 12:28:58 +0300

    moving to post

--- android/views.py
+++ android/views.py
@@ -3,7 +3,7 @@ from Bus import bus
 import json
 
 def lines(request):
-    q = request.GET
+    q = request.POST
     names = bus.get(1,2,3,4,5)
     data = [{'id':key, 'lastStop':value} for key,value in names.items()]
     j = json.dumps({'data':data},ensure_ascii=False)
